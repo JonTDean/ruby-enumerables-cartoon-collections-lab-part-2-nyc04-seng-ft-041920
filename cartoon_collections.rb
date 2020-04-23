76f7e6456f8fffbac518827b661a44d1dee9c28d
Jonathan T Dean <jonathantomdean@gmail.com>
2020-04-23 22:21:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,6 +8,8 @@ end
 def summon_captain_planet(planeteer_calls)
   # Use an Enumerable to capitalize and add '!' to every element in the passed in array
   # Return a new array of the results
+  
+  array.
 end
 
 def long_planeteer_calls(planeteer_calls)
@@ -19,4 +21,12 @@ def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
   # Return the first valid call found, or return nil if no valid calls are found
+  
+  valid_calls.each do |v|
+    if v != nil
+      v.map |bang|
+        v + bang
+      end
+    else
+      nil
 end
